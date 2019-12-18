ee5a23217255faf3797e7930ca5875639f82d45f
kanatos <geoffreydu89@gmail.com>
2019-12-18 23:06:44 +0100

    road back-end

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const connectDB = require("./config/db");
 
 const app = express();
 
@@ -7,14 +6,15 @@ const app = express();
 // connectDB();
 
 // Init Middleware
-// app.use(express.json({ extended: false }));
+//app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactKeeper API." })
 );
 // Define Routes
-// app.use('/api/users', require('./routes/users'));
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
