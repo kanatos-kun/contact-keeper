ca946c8fbd961ad69d97ba7c0ddaba1e25f02c06
kanatos <geoffreydu89@gmail.com>
2020-02-18 22:05:22 +0100

    58 - Contact filter Form & state

---
+++ client/src/component/contacts/ContactFilter.js
@@ -0,0 +1,33 @@
+import React, { useContext, useRef, useEffect } from "react";
+// import PropTypes from "prop-types";
+import ContactContext from "../../context/contact/ContactContext";
+
+const ContactFilter = props => {
+  const contactContext = useContext(ContactContext);
+  const text = useRef("");
+  const { filterContacts, clearFilter, filtered } = contactContext;
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = "";
+    }
+  });
+  const onChange = e => {
+    if (text.current.value !== "") {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+  return (
+    <form>
+      <input
+        ref={text}
+        type="text"
+        placeholder="filter Contacts..."
+        onChange={onChange}
+      />
+    </form>
+  );
+};
+
+export default ContactFilter;
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -30,7 +30,8 @@ const ContactState = props => {
         phone: "222-222-2222"
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -58,19 +59,26 @@ const ContactState = props => {
     dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
   //Filter Contact
-
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   // Clear Filter
-
+  const clearFilter = text => {
+    dispatch({ type: CLEAR_FILTER });
+  };
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {" "}
