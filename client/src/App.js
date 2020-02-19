653aef3782d6ce32c02a89d002353dafbf7fa01f
kanatos <geoffreydu89@gmail.com>
2020-02-19 11:07:12 +0100

    60 - Auth Context & Initial state

--- client/src/App.js
+++ client/src/App.js
@@ -8,22 +8,25 @@ import About from "./component/pages/About";
 // import "./lib/fontawesome/js/all.min.js";
 import NavBar from "./component/layout/NavBar";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <NavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
