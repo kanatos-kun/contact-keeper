58eef91af87ef0cebfd22f98a830f2208cf1aebe
kanatos <geoffreydu89@gmail.com>
2020-02-19 12:43:19 +0100

    62 - Aler Context, State and component

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,39 @@
 import React, { Fragment } from "react";
-import "./App.css";
 import "./lib/fontawesome/css/all.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./component/pages/Home";
 import About from "./component/pages/About";
 import Register from "./component/auth/Register";
 import Login from "./component/auth/Login";
+import Alerts from "./component/layout/Alerts";
 
 // import "./lib/fontawesome/js/all.min.js";
 import NavBar from "./component/layout/NavBar";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import "./App.css";
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <NavBar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <NavBar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/AlertContext";
+
+// import PropTypes from "prop-types";
 
 const Register = props => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -13,7 +19,13 @@ const Register = props => {
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Register submit!");
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields", "danger");
+    } else if (password !== password2) {
+      setAlert("Passwords do not match", "danger");
+    } else {
+      console.log("Register submit !");
+    }
   };
   return (
     <div className="form-container">
@@ -23,11 +35,23 @@ const Register = props => {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange} />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">password</label>
@@ -36,6 +60,8 @@ const Register = props => {
             name="password"
             value={password}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -45,6 +71,8 @@ const Register = props => {
             name="password2"
             value={password2}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <input
